b1ef64c845606d446cf7aa9efc05b39c102bc173
Abhishek Mishra <mishra.abhishek01@hotmail.com>
2024-09-10 08:25:50 +0530

    first commit

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from  '../images/logo.png';
+
+const SignUp = () => {
+  return (
+    <>
+    <div className="container w-screen min-h-screen flex items-center justify-between pl-[100px]">
+      <div className="left">
+        <img src={logo} alt="" />
+
+      </div>
+      <div className="right">
+
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default SignUp
